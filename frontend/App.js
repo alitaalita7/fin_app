2cf787eb7769cd6e55db0d33452d7f1d5da5c818
m1ndos <csgoezkatka@gmail.com>
2024-05-03 18:28:25 +0400

    изменения

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import Achievements from './components/Achievements';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
     <NavigationContainer>
       <View style={styles.container}>
         <Stack.Navigator>
+          <Stack.Screen name="Achievements" component={Achievements} options={{ headerShown: false }} />
           <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
           <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
         </Stack.Navigator>
