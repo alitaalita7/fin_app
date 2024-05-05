9d3c18b70246cbdde2153879e6803ce26a671b72
m1ndos <csgoezkatka@gmail.com>
2024-05-05 19:52:38 +0400

    Добавил таймер, рандом примеры, подсветка правильных/неправильных решений

--- frontend/components/Game.js
+++ frontend/components/Game.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image, Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import Accept from '../assets/game/accept.png'
 import Number_0 from '../assets/game/number_0.png'
@@ -17,12 +17,142 @@ import VA from '../assets/game/VA_coin.png'
 import XP from '../assets/game/xp.png'
 
 const Game = () => {
+
+    // Установка изначальных чисел, знака, и решения
+    const firstNumber = Math.floor(Math.random() * 11)
+    const [num1, setNum1] = useState(firstNumber);
+    const secondNumber = Math.floor(Math.random() * (firstNumber + 1));
+    const [num2, setNum2] = useState(secondNumber);
+    const operators = ['+', '-'];
+    const randomIndex = Math.floor(Math.random() * operators.length);
+    const [sign, setSign] = useState(operators[randomIndex]);
+    let sol = '';
+    if (operators[randomIndex] === '+') {
+        sol = (firstNumber + secondNumber).toString()  
+    } else if (operators[randomIndex] === '-') {
+        sol = (firstNumber - secondNumber).toString()
+    }
+    const [solution, setSolution] = useState(sol);
+
+    const [enteredNumber, setEntederNumber] = useState('')
+    const [schoolClass, setSchoolClass] = useState('');
+    const [counter, setCounter] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [timerId, setTimerId] = useState(null);
+
+    const [color, setColor] = useState('#F7AA2E');
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
+    useEffect(() => {
+        setSchoolClass('1')
+        createExample();
+        const id = setInterval(() => {
+            setSeconds(prevSeconds => prevSeconds + 1);
+        }, 1000);
+        setTimerId(id); // Сохраняем ID интервала в состоянии
+        return () => clearInterval(id);
+    
+    }, [counter]);
+
+    const createExample = () => {
+        switch (schoolClass) {
+            case '1':
+                const firstNumber = Math.floor(Math.random() * 11);
+                setNum1(firstNumber);
+                const secondNumber = Math.floor(Math.random() * (firstNumber + 1));
+                setNum2(secondNumber);
+                const operators = ['+', '-'];
+                const randomIndex = Math.floor(Math.random() * operators.length);
+                setSign(operators[randomIndex]);
+                if (operators[randomIndex] === '+') {
+                    setSolution((firstNumber + secondNumber).toString());
+                } else if (operators[randomIndex] === '-') {
+                    setSolution((firstNumber - secondNumber).toString());
+                }
+                break;
+            case '2':
+
+                break;
+            case '3':
+
+                break;
+            case '4':
+
+                break;
+        }
+    }
+
+    const handlePressAccept = () => {
+        clearInterval(timerId);
+        if (solution === enteredNumber) {
+            setColor('green') // Останавливаем таймер по его ID
+            setTimeout(() => {
+                setColor('#F7AA2E')
+                setEntederNumber('');
+                setSeconds(0);
+                setCounter(counter + 1);
+            }, 2000);
+        } else {
+            setColor('red');
+            setTimeout(() => {
+                setColor('#F7AA2E')
+                setEntederNumber('');
+                setSeconds(0);
+                setCounter(counter + 1);
+            }, 2000);
+        };
+    }
+
+    const inputNumber = (number) => {
+        switch (number) {
+            case Number_1:
+                setEntederNumber(enteredNumber + '1');
+                break;
+            case Number_2:
+                setEntederNumber(enteredNumber + '2');
+                break;
+            case Number_3:
+                setEntederNumber(enteredNumber + '3');
+                break;
+            case Number_4:
+                setEntederNumber(enteredNumber + '4');
+                break;
+            case Number_5:
+                setEntederNumber(enteredNumber + '5');
+                break;
+            case Number_6:
+                setEntederNumber(enteredNumber + '6');
+                break;
+            case Number_7:
+                setEntederNumber(enteredNumber + '7');
+                break;
+            case Number_8:
+                setEntederNumber(enteredNumber + '8');
+                break;
+            case Number_9:
+                setEntederNumber(enteredNumber + '9');
+                break;
+            case Number_0:
+                setEntederNumber(enteredNumber + '0');
+                break;
+            case Remove:
+                setEntederNumber(enteredNumber.slice(0, -1));
+                break;
+        }
+
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <View style={styles.timer}>
                     <Image source={Timer} style={{ width: 60, height: 60 }}></Image>
-                    <Text style={{ fontSize: 40, fontWeight: '300' }}>00:00</Text>
+                    <Text style={{ fontSize: 40, fontWeight: '300' }}>{formatTime(seconds)}</Text>
                 </View>
                 <View style={styles.viewXpVa}>
                     <View style={styles.XpVa}>
@@ -37,46 +167,53 @@ const Game = () => {
             </View>
             <View style={styles.viewExample}>
                 <Text style={{ color: "#F7AA2E", fontSize: 100, fontWeight: '700' }}>
-                    2 × 2 =
-                    <Text> </Text>
-                    <Text style={{ textDecorationLine: 'underline' }}>5</Text>
+                    <Text>{num1} </Text>
+                    <Text>{sign} </Text>
+                    <Text>{num2} </Text>
+                    <Text>= </Text>
+                    {enteredNumber ? (
+                        <Text style={{ textDecorationLine: 'underline', color: color}}>{enteredNumber}</Text>
+                    ) : (
+                        <Text style={{color: color}}>__</Text>
+                    )}
+
                 </Text>
             </View>
             <View style={styles.viewButtons}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_1)}>
                     <Image source={Number_1} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_2)}>
                     <Image source={Number_2} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_3)}>
                     <Image source={Number_3} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_4)}>
                     <Image source={Number_4} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_5)}>
                     <Image source={Number_5} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_6)}>
                     <Image source={Number_6} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_7)}>
                     <Image source={Number_7} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_8)}>
                     <Image source={Number_8} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_9)}>
                     <Image source={Number_9} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Number_0)}>
                     <Image source={Number_0} style={styles.imageButtons}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => inputNumber(Remove)}>
                     <Image source={Remove} style={{ width: 119, height: 60, marginLeft: 10 }}></Image>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handlePressAccept}>
                     <Image source={Accept} style={{ width: 60, height: 60, marginLeft: 10 }}></Image>
                 </TouchableOpacity>
             </View>
